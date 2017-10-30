116ab5c4c0532e28b28dffe03112a3798553b70a
Juan D. Bolaños <bolanosjd@gmail.com>
2017-10-30 15:58:43 -0400

    Creating seperate dir for hard coded site

---
+++ front_end/js/app.js
@@ -0,0 +1,12 @@
+/*!
+ * Synchrony Group 1.0
+ * @license MIT licensed
+ *
+ * Copyright (C) 2017 synchronygroup.com - A project by Juan D. Bolaños
+ */
+
+$(document).ready(function() {
+
+	
+
+});
